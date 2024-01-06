582e8bc0bd8ba00589820393e54f4abccfbc090b
yūgu <118666092+geekyTWILIGHT@users.noreply.github.com>
2024-01-07 01:25:54 +0800

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,449 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="homepage.css">
+    <title>Header HP</title>
+</head>
+<body>
+    
+
+    <!-- Header -->
+    <header class="header_homepage">
+        <div class="website_logo_container">
+            <div class="website_logo_contents">
+                <img id="website_logo_hp" src="graphics/stiLogo.png" alt="website logo">
+            </div>
+        </div>
+        <div class="header_hp_nav_bars">
+            <div class="header_hp_our_story_nav_bar">
+                <a id="header_hp_our_story">Our story</a>
+            </div>
+            <div class="header_hp_contact_nav_bar">
+                <a id="header_hp_contact">Contact</a>
+            </div>
+        </div>
+        <div class="header_hp_logged_in_acc">
+            <div class="header_hp_logged_in_username_container">
+                <h3 id="header_hp_logged_in_username">UserName</h3>
+            </div>
+            <div class="header_hp_logged_in_dropdown">
+                <div class="header_hp_logged_in_dropdown_container" id="header_hp_dropdown">
+                    <button id="header_hp_logged_in_dropdown_icon">&#709;</button>
+                </div>
+                <div class="header_hp_logged_in_account_info" id="header_hp_dropdown_contents">
+                    <div class="header_hp_logged_in_email_container">
+                        <div class="header_hp_logged_in_email_contents">
+                            <h3 id="header_hp_logged_in_email">UserEmail</h3>
+                        </div>
+                    </div>
+                    <div class="header_hp_logged_in_logout_btn_container">
+                        <div class="header_hp_logged_in_logout_btn_contents">
+                            <p id="header_hp_logged_in_logout_btn">Log Out</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="header_hp_not_logged_in_acc">
+            <div class="log_in_container">
+                <button class="log_in_btn">Log In</button>
+            </div>
+            <div class="sign_up_container">
+                <button class="sign_up_btn">Sign Up</button>
+            </div>
+        </div> -->
+    </header>
+
+
+    <!-- Introduction (Landing Page) -->
+    <div class="intro_page">
+        <div class="intro_contents">
+            <span>Affordable </span>
+            <span style="color: #ffc107;">STI UNIFORMS</span>
+            <br>
+            <span>now available </span>
+            <span style="color: #01579B;">Online</span>
+            <br>
+            <br>
+            <div class="setText">
+                <span style="font-family: Helvetica; color: #737373; font-size: 16px; text-transform: none;">
+                    We take pride in providing the </span> <span style="color: #01579B; font-family: Helvetica; font-size: 16px; 
+                    text-transform: none;">highest quality</span> <span style="font-family: Helvetica; font-size: 16px; color: #737373; 
+                    text-transform: none;"> uniforms at STI College, meticulously crafted to meet your needs. Now, these exceptional uniforms are
+                     readily available for convenient online purchase, ensuring you have easy access to the perfect attire for your academic journey.
+                </span>
+            </div>
+            <br>
+            <button id="purchase_button" type="button">Purchase Now</button>
+        </div>
+    </div>
+
+
+    <!-- 1sr Content (sti branch list) -->
+    <div class="branches_supplied">
+        <div class="branch_sup_title_contain">
+            <h3 id="branch_sup_title">Branches we currently supply</h3>
+        </div>
+        <div class="branches">
+            <div class="branch" id="alabang">
+                <p class="name">Alabang</p>
+            </div>
+            <div class="branch" id="batangas">
+                <p class="name">Batangas</p>
+            </div>
+            <div class="branch" id="caloocan">
+                <p class="name">Caloocan</p>
+            </div>
+            <div class="branch" id="cubao">
+                <p class="name">Cubao</p>
+            </div>
+            <div class="branch" id="fairview">
+                <p class="name">Fairview</p>
+            </div>
+            <div class="branch" id="global_city">
+                <p class="name">Global city</p>
+            </div>
+            <div class="branch" id="iligan">
+                <p class="name">Iligan</p>
+            </div>
+            <div class="branch" id="munoz_edsa">
+                <p class="name">Munoz-edsa</p>
+            </div>
+            <div class="branch" id="naga">
+                <p class="name">Naga</p>
+            </div>
+            <div class="branch" id="namei">
+                <p class="name">Namei</p>
+            </div>
+            <div class="branch" id="novaliches">
+                <p class="name">Novaliches</p>
+            </div>
+            <div class="branch" id="sta_mesa">
+                <p class="name">Sta. mesa</p>
+            </div>
+        </div>
+    </div>
+    
+
+    <!-- 2nd Content (number of something) -->
+    <div class="number_of">
+        <div class="num_of_container">
+            <div class="num_of_contents">
+                <div class="num_of_integer_container">
+                    <h3 class="num_of_integer">500+</h3>
+                </div>
+                <div class="num_of_title_container">
+                    <h3 class="num_of_title">Uniforms sold</h3>
+                </div>
+            </div>
+        </div>
+        <div class="num_of_container">
+            <div class="num_of_contents">
+                <div class="num_of_integer_container">
+                    <h3 class="num_of_integer">300+</h3>
+                </div>
+                <div class="num_of_title_container">
+                    <h3 class="num_of_title">Students catered</h3>
+                </div>
+            </div>
+        </div>
+        <div class="num_of_container">
+            <div class="num_of_contents last_num_of_contents">
+                <div class="num_of_integer_container">
+                    <h3 class="num_of_integer">10+</h3>
+                </div>
+                <div class="num_of_title_container">
+                    <h3 class="num_of_title">Branches supplied</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- 3rd Content (what we offer) -->
+    <div class="we_provide">
+        <div class="we_prov_title_container">
+            <hr id="we_prov_title_line_left">
+            <h3 id="we_prov_title">What we provide</h3>
+            <hr id="we_prov_title_line_right">
+        </div>
+        <div class="we_provide_contents">
+            <div class="we_prov_contents_container">
+                <div class="we_prov_cards">
+                    <div class="we_prov_icon_container">
+                        <img class="we_prov_icon" src="icons/checkShield.png" alt="icon">
+                    </div>
+                    <h3 class="we_prov_sub_title">Reliable delivery services</h3>
+                    <p class="we_prov_details">
+                        We provide efficient and reliable delivery services, 
+                        ensuring prompt and seamless distribution anywhere you need
+                    </p>
+                </div>
+                <div class="we_prov_cards">
+                    <div class="we_prov_icon_container">
+                        <img class="we_prov_icon" src="icons/bestQuality.jpg" alt="icon">
+                    </div>
+                    <h3 class="we_prov_sub_title">High quality uniform</h3>
+                    <p class="we_prov_details">
+                        We provide tailored, high-quality uniforms for your academic needs, ensuring a professional and 
+                        standardized appearance across various academic settings.
+                    </p>
+                </div>
+                <div class="we_prov_cards">
+                    <div class="we_prov_icon_container">
+                        <img class="we_prov_icon" src="icons/time.png" alt="icon">
+                    </div>
+                    <h3 class="we_prov_sub_title">Efficient online platform</h3>
+                    <p class="we_prov_details">
+                        We provide a simple and efficient platform for convenient ordering of uniforms designed to meet your specific requirements
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- 4th Content (made by) -->
+    <div class="sti_nova_made">
+        <div class="sti_nova_profile">
+            <img id="sti_nova_logo" src="graphics/stiNovalichesLogo.jpg">
+            <h3 id="sti_nova_name">STI Novaliches</h3>
+        </div>
+        <div class="sti_nova_information">
+            <p id="sti_nova_information_text">
+                The Stylist online platform, <span> created by a group of students from the STI Novaliches</span>, 
+                is a platform <span> exclusively </span> for students of the <span> Systems Technology Institute (STI) </span>. 
+                It facilitates <span> convenient purchase and delivery of STI uniforms </span>
+            </p>
+        </div>
+    </div>
+
+
+    <!-- 5th Content (what the platform can do) -->
+    <div class="what_our_service_do">
+        <div class="wht_sv_do_title_contain">
+            <p id="what_plat_do">What it can do</p>
+            <p id="wht_sv_do_title">Explore <span> the functionality of our platform </span> </p>
+        </div>
+        <div class="wht_sv_do_buttons">
+            <div class="wht_sv_do_line_des">
+            </div>
+            <div class="wht_sv_do_btn_contain">
+                <button class="sv_button" id="sv_prev_btn">&lt;</button>
+                <button class="sv_button" id="sv_next_btn">&gt;</button>
+            </div>
+        </div>
+        <div class="wht_sv_do_contents" id="wht_sv_do_cont">
+            <div class="wht_sv_do_cont_contents" id="wht_sv_do_cards">
+                <div class="sv_do_card" id="sv_do_first">
+                    <div class="sv_do_icon">
+                        <img class="icon" src="icons/filter.png" alt="icon">
+                    </div>
+                    <div class="sv_do">
+                        <h3 class="do">Filter function</h3>
+                    </div>
+                    <div class="sv_do_details">
+                        <p class="details">
+                            Effortlessly navigate through a diverse array of 
+                            products with intuitive filtering options, streamlining your 
+                            search for the perfect item.
+                        </p>
+                    </div>
+                </div>
+                <div class="sv_do_card" id="sv_do_second">
+                    <div class="sv_do_icon">
+                        <img class="icon" src="icons/flexibity.png" alt="icon">
+                    </div>
+                    <div class="sv_do">
+                        <h3 class="do">Flexible delivery options</h3>
+                    </div>
+                    <div class="sv_do_details">
+                        <p class="details">
+                            Customize your delivery experience by selecting from various 
+                            delivery methods tailored to suit your preferences.
+                        </p>
+                    </div>  
+                </div>
+                <div class="sv_do_card" id="sv_do_third">
+                    <div class="sv_do_icon">
+                        <img class="icon" src="icons/customizable.png" alt="icon">
+                    </div>
+                    <div class="sv_do">
+                        <h3 class="do">Customizable orders</h3>
+                    </div>
+                    <div class="sv_do_details">
+                        <p class="details">
+                            Enjoy the freedom to tailor your preferences and personalize your 
+                            orders according to your unique tastes and preferences.
+                        </p>
+                    </div>  
+                </div>
+                <div class="sv_do_card" id="sv_do_fourth">
+                    <div class="sv_do_icon">
+                        <img class="icon" src="icons/secure.jpg" alt="icon">
+                    </div>
+                    <div class="sv_do">
+                        <h3 class="do">Safe and secure</h3>
+                    </div>
+                    <div class="sv_do_details">
+                        <p class="details">
+                            Safe and secure online transactions that protect sensitive 
+                            information and financial assets during digital exchanges.
+                        </p>
+                    </div> 
+                </div>
+                <div class="sv_do_card" id="sv_do_fifth">
+                    <div class="sv_do_icon">
+                        <img class="icon" src="icons/customer.png" alt="icon">
+                    </div>
+                    <div class="sv_do">
+                        <h3 class="do">Customer support</h3>
+                    </div>
+                    <div class="sv_do_details">
+                        <p class="details">
+                            Connect with us through multiple communication channels for 
+                            convenient and flexible interaction.
+                        </p>
+                    </div> 
+                </div>
+                <div class="sv_do_card" id="sv_do_sixth">
+                    <div class="sv_do_icon">
+                        <img class="icon" src="icons/time.png" alt="icon">
+                    </div>
+                    <div class="sv_do">
+                        <h3 class="do">Realtime transaction</h3>
+                    </div>
+                    <div class="sv_do_details">
+                        <p class="details">
+                            Automated system for real-time computation and processing of purchased
+                            amounts, enabling fast and efficient transaction. 
+                        </p>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- 6th Content (featured item) -->
+    <div class="featured_products">
+        <div class="featured_pro_contents_container">
+            <div class="featured_pro_contents">
+                <h3 id="featured_pro_header">Go and purchase the STI 40th anniversary shirt</h3>
+                <p id="featured_pro_details">
+                    Founded on August 21, 1983, by Augusto C. Lagman, Herman T. Gamboa, Benjamin A. Santos, and Edgar H. 
+                    Sarte, the Systems Technology Institute, commonly known as STI, commemorates its 40th year with a 
+                    specially designed anniversary shirt—get yours now to celebrate this milestone.
+                </p>
+                <button id="featured_pro_btn">Go</button>
+            </div>
+        </div>
+        <div class="featured_pro_image_container">
+            <div class="featured_pro_image_content">
+                <img id="featued_pro_image" src="graphics/featuredItem.jpg" alt="featured_item">
+            </div>
+        </div>
+    </div>
+
+
+    <hr class="horizontal_line">
+
+
+    <!-- 7th Content (get in touch) -->
+    <div class="get_in_touch">
+        <div class="get_in_touch_bg">
+            <div class="get_in_touch_container">
+                <h3 id="get_in_touch_title">Curious about something?</h3>
+                <button id="get_in_touch_btn">Get in touch</button>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Footer -->
+    <footer class="footer">
+        <hr id="footer_separator1">
+        <div class="footer_contents">
+            <div class="website_id">
+                <h3 id="site_name">The STylIst</h3>
+                <img id="logo" src="graphics/stiLogo.png" alt="STylIst Logo">
+            </div>
+            <div class="information">
+                <h3 id="information_title">Information</h3>
+                <div class="information_contents">
+                    <a id="about_us" class="footer_links">About Us</a>
+                    <a id="our_story" class="footer_links">Our Story</a>
+                </div>
+            </div>
+            <div class="help">
+                <h3 id="help_title">Help</h3>
+                <div class="help_contents">
+                    <a id="contact_us" class="footer_links">Contact Us</a>
+                    <a id="faqs" class="footer_links">FAQs</a>
+                    <a id="return_policy" class="footer_links">Return Policy</a>
+                    <a id="terms_of_service" class="footer_links">Terms of Service</a>
+                    <a id="privacy_policy" class="footer_links">Privacy Policy</a>
+                </div>  
+            </div>
+            <div class="connect">
+                <h3 id="connect_title">Connect</h3>
+                <div class="connect_contents">
+                    <a id="instagram" class="footer_links">Instagram</a>
+                    <a id="tiktok" class="footer_links">TikTok</a>
+                    <a id="facebook" class="footer_links">Facebook</a>
+                    <a id="twitter" class="footer_links">Twitter</a>
+                    <a id="youtube" class="footer_links">Youtube</a>
+                </div>
+            </div>
+        </div>
+        <hr id="footer_separator2">
+        <div class="website_info">
+            <h3 id="copyright">© 2024, <span id="website_link">The Stylist</span></h3>
+        </div>
+    </footer>
+
+
+<script>
+
+    // For: Header 
+    const headerDropdown = document.getElementById('header_hp_dropdown');
+    const headerDropdownContents = document.getElementById('header_hp_dropdown_contents');
+
+    headerDropdown.addEventListener('click', function() {
+        if (headerDropdownContents.style.display === 'none') {
+            headerDropdownContents.style.display = 'flex';
+        }
+        else {
+            headerDropdownContents.style.display = 'none';
+        }
+    });
+
+
+    // For: 5th Content (what the platform can do)
+    const whtSvDoContainer = document.getElementById('wht_sv_do_cont');
+    const whtSvDoCards = document.getElementById('wht_sv_do_cards');
+    const whtSvDoPrevBtn = document.getElementById('sv_prev_btn');
+    const whtSvDoNextBtn = document.getElementById('sv_next_btn');
+    let whtSvDoCardsScrollPos = 0;
+
+    whtSvDoPrevBtn.addEventListener('click', () => {
+        whtSvDoCardsScrollPos -= whtSvDoCards.clientWidth / 4; 
+        scrollContent();
+    });
+
+    whtSvDoNextBtn.addEventListener('click', () => {
+        whtSvDoCardsScrollPos += whtSvDoCards.clientWidth / 4; 
+        scrollContent();
+    });
+
+    function scrollContent() {
+        whtSvDoCardsScrollPos = Math.max(0, Math.min(whtSvDoCardsScrollPos, whtSvDoCards.scrollWidth - whtSvDoContainer.clientWidth));
+        whtSvDoCards.style.transform = `translateX(-${whtSvDoCardsScrollPos}px)`;
+    }
+
+</script>
+
+</body>
+</html>
